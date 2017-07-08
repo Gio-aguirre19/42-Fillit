f6f33ac12ede932bda4e01e58eae10272d03a25e
Giovani Aguirre <aguirregio19@gmail.com>
2017-07-08 15:27:50 -0700

    Added puzzle pieces to functions

--- get_puzzle.c
+++ get_puzzle.c
@@ -1,19 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   create_tetri.c                                     :+:      :+:    :+:   */
+/*   get_puzzle.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaguirre <gio_aguirre19@yahoo.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 20:43:18 by gaguirre          #+#    #+#             */
-/*   Updated: 2017/07/06 23:09:09 by gaguirre         ###   ########.fr       */
+/*   Updated: 2017/07/08 15:27:09 by gaguirre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		gridcpy(char *grid, char *file, int i)
+#include <fillit.h>
+
+int		count_pieces(char *file)
 {
+	int i;
+	int piece;
+
+	i = 0;
+	piece = 0;
+	while (file[i])
+	{
+		if (file[i] == '\n')
+		{
+			if (file[i + 1] == '\n' || file[i + 1] == '\0')
+				piece++;
+		}
+		i++;
+	}
+	return (piece);
+}
+
+int		create_piece(char *grid, char *file)
+{
+	int i;
 	int j;
 
+	i = 0;
 	j = 0;
 	while (j <= 16)
 	{
@@ -23,35 +46,36 @@ int		gridcpy(char *grid, char *file, int i)
 			j++;
 		}
 		if (file[i] == '\n')
-		{
 			if (file[i + 1] == '\n' || file[i + 1] == '\0')
 			{
-				grid[j++] = '\0';
-				return (i++);
+				grid[j] = '\0';
+				return (1);
 			}
-		}
-		i++;
 	}
+	i++;
 }
 
-char	**grid(char *file, int num)
+int		get_pieces(char *file, char ** puzzle)
 {
 	int i;
 	int j;
-	char **grid;
 
 	i = 0;
 	j = 0;
-	grid = (char **)malloc(sizeof(char) * num + 1);
-	if (!grid)
-		return (0);
 	while (file[i])
 	{
-		grid[j] = (char *)malloc(sizeof(char) * 16 + 1);
-		if (!(grid[j]))
+		puzzle[j] = (char *)malloc(sizeof(char) * 16 + 1);
+		if (!(puzzle[j]))
 			return (0);
-		gridcpy(grid[j], file[i], i);
+		create_piece(puzzle[j], &file[i]);
+		i += 21;
+		ft_putstr(puzzle[j]);
+		ft_putchar('\n');
 		j++;
 	}
-	return (grid);
+	puzzle[j] = (char *)malloc(sizeof(char));
+	puzzle[j][0] = '\0';
+	ft_putstr(puzzle[j]);
+	ft_putchar('\n');
+	return (1);
 }
